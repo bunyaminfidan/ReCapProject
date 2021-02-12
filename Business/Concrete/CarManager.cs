21215d3e20d8efb35c5e696e3e4c493f394734b3
BILTEK\386961 <bunyamnfdan@gmail.com>
2021-02-12 09:11:35 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Abstract;
+using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public List<Car> GetAll()
+        {
+          return  _carDal.GetAll();
+        }
+
+        public List<Car> GetCarById(int carId)
+        {
+            return _carDal.GetCarById(carId);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+}
+
+        }
+    }
