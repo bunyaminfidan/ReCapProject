f0bf8cb2879f37603484d697f55bf0fcd012f325
BILTEK\386961 <bunyamnfdan@gmail.com>
2021-02-26 08:28:00 +0300

    resim upload işlemlerine başlandı

--- Business/Concrete/CarImageManager.cs
+++ Business/Concrete/CarImageManager.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Abstract;
 using Core.Utilitis.Results;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,13 @@ namespace Business.Concrete
 {
     class CarImageManager : ICarImageService
     {
+        ICarImageDal _carImageDal;
+
+        public CarImageManager(ICarImageDal carImageDal)
+        {
+            _carImageDal = carImageDal;
+        }
+
         public IResult Add(CarImage carImage)
         {
             throw new NotImplementedException();
